7e2b27d59669191c8092e67e4d9c623bded64160
KingNono1030 <hoejy0821@gmail.com>
2024-09-03 11:42:50 +0900

    chore/테일윈드 config 사이즈 단위 rem -> px

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,46 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
+
+const px0_10 = Array.from(Array(11)).reduce(
+  (acc, _, i) => {
+    acc[i] = `${i}px`;
+    return acc;
+  },
+  {} as Record<number, string>
+);
+
+const px0_100 = Array.from(Array(101)).reduce(
+  (acc, _, i) => {
+    acc[i] = `${i}px`;
+    return acc;
+  },
+  {} as Record<number, string>
+);
+
+const px0_200 = Array.from(Array(201)).reduce(
+  (acc, _, i) => {
+    acc[i] = `${i}px`;
+    return acc;
+  },
+  {} as Record<number, string>
+);
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      borderWidth: px0_10,
+      fontSize: px0_100,
+      lineHeight: px0_100,
+      minWidth: px0_200,
+      minHeight: px0_200,
+      spacing: px0_200,
+      margin: px0_100,
+      padding: px0_100,
+      gap: px0_10,
     },
   },
   plugins: [],
