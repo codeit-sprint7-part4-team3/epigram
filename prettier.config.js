f773de7ab752f3d8bf4f2b844503f88f62d72ca1
KingNono1030 <hoejy0821@gmail.com>
2024-09-03 09:16:56 +0900

    chore/프리티어 config 추가

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  printWidth: 80,
+  trailingComma: 'es5',
+  tabWidth: 2,
+  useTabs: false,
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  plugins: ['prettier-plugin-tailwindcss'],
+}
