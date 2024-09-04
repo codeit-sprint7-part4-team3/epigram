bd58c305ac9d8be733947a82cfef468f52da659f
KingNono1030 <hoejy0821@gmail.com>
2024-09-04 15:24:18 +0900

    chore/cdn 링크 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,20 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang='ko'>
-      <Head />
+      <Head>
+        <link
+          rel='preload'
+          as='style'
+          href='https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css'
+        />
+        <link
+          rel='preload'
+          as='style'
+          href='https://cdn.jsdelivr.net/font-iropke-batang/1.2/font-iropke-batang.css'
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
