b24ced33d9f2a5387e18b034161b449559165a1d
KingNono1030 <hoejy0821@gmail.com>
2024-09-05 03:17:14 +0900

    feat/버튼 스타일링

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,14 @@
+import clsx from 'clsx';
 import { ButtonHTMLAttributes } from 'react';
 
-type ButtonVariant = 'main' | 'wide' | 'default';
+type ButtonVariant = 'main' | 'wide' | 'round';
+type ButtonSize = 'sm' | 'md';
+type ButtonColor = 'primary' | 'white' | 'blue';
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   variant: ButtonVariant;
+  size: ButtonSize;
+  color: ButtonColor;
 }
 
 export default function Button({
@@ -13,11 +18,20 @@ export default function Button({
   form,
   onClick,
   disabled = false,
-  variant = 'default',
+  variant = 'main',
+  size = 'md',
+  color = 'primary',
 }: ButtonProps) {
+  const buttonStyle = clsx(
+    'flex-center',
+    styleByVariant[variant],
+    styleByColor[color],
+    size === 'sm' && smButtonStyle,
+    className
+  );
   return (
     <button
-      className={className}
+      className={buttonStyle}
       type={type}
       form={form}
       onClick={onClick}
@@ -28,8 +42,27 @@ export default function Button({
   );
 }
 
+const baseStyle = 'rounded-xl font-semibold';
+
 const styleByVariant: Record<ButtonVariant, string> = {
-  main: '',
-  wide: '',
-  default: '',
+  main: clsx(
+    baseStyle,
+    'w-112 h-48 md:w-136 md:h-56 xl:w-286 xl:h-64 text-16 leading-26 md:text-20 md:leading-32'
+  ),
+  wide: clsx(
+    baseStyle,
+    'w-full h-44 xl:h-64 text-16 leading-26 xl:text-20 xl:leading-32'
+  ),
+  round:
+    'h-48 xl:h-56 px-18 xl:px-40 text-14 font-medium leading-24 xl:text-20 xl:leading-32',
 };
+
+const styleByColor: Record<ButtonColor, string> = {
+  primary:
+    'bg-black-500 text-blue-100 hover:bg-black-600 active:bg-black-700 disabled:bg-blue-300 disabled:outline disabled:outline-blue-100',
+  white: 'bg-background-100 border border-line-200 text-blue-500',
+  blue: 'bg-blue-900 text-blue-100',
+};
+
+const smButtonStyle =
+  'w-auto px-16 h-32 md:h-44 text-12 leading-20 md:text-16 md:leading-26';
