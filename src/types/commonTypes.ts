c593a4a2d2c24407e83c8ce317619f9e4df7eac7
KingNono1030 <hoejy0821@gmail.com>
2024-09-06 11:18:20 +0900

    feat/공용 타입 정의

---
+++ src/types/commonTypes.ts
@@ -0,0 +1,8 @@
+export type Id = number; // Format: int32
+export type Nickname = string;
+export type UrlType = string; // Format: URL
+
+export interface Timestamps {
+  createdAt: string; // Format: date-time
+  updatedAt: string; // Format: date-time
+}
