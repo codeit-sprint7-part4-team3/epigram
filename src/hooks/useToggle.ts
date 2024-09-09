96eaac27e5704be33732daf39dc40a39747ca2cb
KingNono1030 <hoejy0821@gmail.com>
2024-09-10 08:44:52 +0900

    refactor/useToggle 훅 반환값 타입 정의

--- src/hooks/useToggle.ts
+++ src/hooks/useToggle.ts
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-export default function useToggle() {
+export default function useToggle(): [boolean, () => void] {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggle = () => {
