0320c5337412fc9646aa954d7536285eb6a31334
soyoung <ssh0040233@gmail.com>
2024-09-11 06:20:04 +0900

    refactor/줄무늬 배경 tailwind apply 적용

--- src/shared/EpigramCard.tsx
+++ src/shared/EpigramCard.tsx
@@ -7,7 +7,7 @@ interface CardProps {
 export default function EpigramCard({ content, author, tags }: CardProps) {
   return (
     <div className='font-secondary'>
-      <div className='bg-stripe-pattern bg-stripe-size mb-8 flex min-h-295 flex-col justify-between rounded-2xl bg-blue-100 px-24 py-23'>
+      <div className='stripe-pattern mb-8 flex min-h-295 flex-col justify-between rounded-2xl bg-blue-100 px-24 py-23'>
         <div className='flex-1'>
           <div className='text-14 font-normal leading-24 md:text-16 md:leading-26 xl:text-24 xl:leading-40'>
             {content}
