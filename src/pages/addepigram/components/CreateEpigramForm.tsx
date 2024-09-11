396f794bc89adefee93fa56d588b1d720bf809a7
KingNono1030 <hoejy0821@gmail.com>
2024-09-11 12:04:12 +0900

    feat/에피그램 생성 폼 1/2 완성

--- src/pages/addepigram/components/CreateEpigramForm.tsx
+++ src/pages/addepigram/components/CreateEpigramForm.tsx
@@ -1,54 +1,81 @@
 import Form from '@/components/Form';
+import useToggle from '@/hooks/useToggle';
 
 export default function CreateEpigramForm() {
-  // const values: CreateEpigramBody = {
-  //   tags,
-  //   author,
-  //   content,
-  //   referenceTitle,
-  //   referenceUrl,
-  // };
+  const [isInputActive, , deactivateInput, activateInput] = useToggle(false);
   return (
-    <div className='mx-auto w-full md:max-w-384 xl:max-w-640'>
-      <Form
-        onSubmit={(data: CreateEpigramBody) => {
-          console.log('에피그램 생성 폼 제출');
-          console.log(data);
-        }}
-      >
-        <Form.Label className='mb-40 xl:mb-54'>
-          <Form.LabelHeader className='mb-8 font-semibold xl:mb-24'>
-            내용<span className='ml-4 font-medium text-error xl:ml-6'>*</span>
-          </Form.LabelHeader>
-          <Form.TextArea
-            name='content'
-            className='min-h-132 xl:min-h-148'
-            placeholder='500자 이내로 입력해주세요.'
-            required
-          />
-        </Form.Label>
-        <div className='mb-40 xl:mb-54'>
-          <Form.LabelHeader className='mb-8 font-semibold xl:mb-24'>
-            저자<span className='ml-4 font-medium text-error xl:ml-6'>*</span>
-          </Form.LabelHeader>
-          <div className='mb-12 flex gap-16 xl:mb-16 xl:gap-24'>
-            <Form.Label className='flex items-center gap-8'>
-              <Form.RadioInput name='author' value='직접 입력' checked />
-              직접 입력
-            </Form.Label>
-            <Form.Label className='flex items-center gap-8'>
-              <Form.RadioInput name='author' value='알 수 없음' />알 수 없음
-            </Form.Label>
-            <Form.Label className='flex items-center gap-8'>
-              <Form.RadioInput name='author' value='본인' />
-              본인
-            </Form.Label>
-          </div>
-          <Form.Input name='author' placeholder='저자 이름 입력' disabled />
+    <Form
+      onSubmit={(data: CreateEpigramBody) => {
+        console.log('에피그램 생성 폼 제출');
+        console.log(data);
+      }}
+    >
+      <Form.Label className='mb-40 xl:mb-54'>
+        <Form.LabelHeader className='mb-8 font-semibold xl:mb-24'>
+          내용<span className='ml-4 font-medium text-error xl:ml-6'>*</span>
+        </Form.LabelHeader>
+        <Form.TextArea
+          name='content'
+          className='min-h-132 xl:min-h-148'
+          placeholder='500자 이내로 입력해주세요.'
+          required
+        />
+      </Form.Label>
+      <div className='mb-40 xl:mb-54'>
+        <Form.LabelHeader className='mb-8 font-semibold xl:mb-24'>
+          저자<span className='ml-4 font-medium text-error xl:ml-6'>*</span>
+        </Form.LabelHeader>
+        <div className='mb-12 flex gap-16 text-base font-medium leading-26 text-black-600 xl:mb-16 xl:gap-24 xl:text-xl xl:leading-32'>
+          <Form.Label
+            className='flex cursor-pointer items-center gap-8'
+            onClick={() => {
+              activateInput();
+              console.log('dd');
+            }}
+          >
+            <Form.RadioInput name='author' value='직접 입력' />
+            직접 입력
+          </Form.Label>
+          <Form.Label
+            className='flex cursor-pointer items-center gap-8'
+            onClick={deactivateInput}
+          >
+            <Form.RadioInput name='author' value='알 수 없음' />알 수 없음
+          </Form.Label>
+          <Form.Label
+            className='flex cursor-pointer items-center gap-8'
+            onClick={deactivateInput}
+          >
+            <Form.RadioInput name='author' value='본인' />
+            본인
+          </Form.Label>
         </div>
-
-        <Form.Submit>가입하기</Form.Submit>
-      </Form>
-    </div>
+        <Form.Input
+          className='disabled:cursor-not-allowed'
+          name='author'
+          placeholder='저자 이름 입력'
+          disabled={!isInputActive}
+        />
+      </div>
+      <Form.Label className='mb-8 xl:mb-16'>
+        <Form.LabelHeader className='mb-8 font-semibold xl:mb-24'>
+          출처
+        </Form.LabelHeader>
+        <Form.Input name='referenceTitle' placeholder='출처 제목 입력' />
+      </Form.Label>
+      <Form.Label className='mb-40 xl:mb-54'>
+        <Form.Input
+          name='referenceUrl'
+          placeholder='URL (ex. https://www.website.com)'
+        />
+      </Form.Label>
+      <Form.Label className='mb-40 xl:mb-54'>
+        <Form.LabelHeader className='mb-8 font-semibold xl:mb-24'>
+          태그
+        </Form.LabelHeader>
+        <Form.Input name='tags' placeholder='입력하여 태그 작성 (최대 10자)' />
+      </Form.Label>
+      <Form.Submit>가입하기</Form.Submit>
+    </Form>
   );
 }
