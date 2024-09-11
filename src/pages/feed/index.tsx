60ffffe361aa5eda081ef025e78556b9b9c458b3
soyoung <ssh0040233@gmail.com>
2024-09-12 08:24:40 +0900

    feat/에피그램 카드 더보기(4개씩)

--- src/pages/feed/index.tsx
+++ src/pages/feed/index.tsx
@@ -5,30 +5,7 @@ import EpigramCard from '@/shared/EpigramCard';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 
-const mockDataArray = [
-  { content: '명언 1입니다', author: 'Author One', tags: ['첫번째'] },
-  {
-    content: `Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work. And the only way to do great work is to love what you do. If you haven't found it yet, keep looking. Don't settle. As with all matters of the heart, you'll know when you find it`,
-    author: 'Steve Jobs',
-    tags: ['긴 명언 테스트', '스크롤로 표시?', '그냥 길어지도록?'],
-  },
-  {
-    content:
-      'If you cannot fly then run. If you cannot run, then walk. And, if you cannot walk, then crawl, but whatever you do, you have to keep moving forward.',
-    author: 'Martin Luther King Jr',
-    tags: ['태그2', '태그4'],
-  },
-  {
-    content: 'Yet another piece of mock content',
-    author: 'Author Three',
-    tags: ['태그3'],
-  },
-  {
-    content: 'Yet another piece of mock content',
-    author: 'Author Three',
-    tags: ['태그4'],
-  },
-];
+import { mockDataArray } from './mockData';
 
 export default function Feed() {
   const router = useRouter();
@@ -37,15 +14,19 @@ export default function Feed() {
     if (router.pathname === '/addepigram') return;
     router.push('/addepigram');
   };
+
   const handlePageUp = () => {
     window.scrollTo({
       top: 0,
       behavior: 'smooth',
     });
   };
+
   const [cards, setCards] = useState<
     { content: string; author: string; tags: string[] }[]
   >([]);
+
+  const [visibleCount, setVisibleCount] = useState(6);
 
   useEffect(() => {
     setTimeout(() => {
@@ -53,14 +34,18 @@ export default function Feed() {
     }, 500);
   }, []);
 
+  const handleLoadMore = () => {
+    setVisibleCount(prevCount => prevCount + 4);
+  };
+
   return (
-    <div className='h-full w-full bg-background-100'>
-      <div className='px-24 md:px-72 xl:px-360'>
+    <div className='h-full w-full bg-background-100 pb-100'>
+      <div className='transition-animation px-24 md:px-72 xl:px-360'>
         <h1 className='pb-24 pt-32 font-primary text-24 font-semibold md:pb-40 md:pt-120 md:leading-32'>
           피드
         </h1>
         <div className='grid grid-cols-1 gap-x-8 gap-y-16 md:grid-cols-2 md:gap-x-12 md:gap-y-24 xl:gap-x-30 xl:gap-y-40'>
-          {cards.map((card, index) => (
+          {cards.slice(0, visibleCount).map((card, index) => (
             <EpigramCard
               key={index}
               content={card.content}
@@ -70,12 +55,14 @@ export default function Feed() {
           ))}
         </div>
       </div>
-      <div className='pt:56 flex items-center justify-center pb-114 xl:pt-80'>
-        <Button variant='round' color='white'>
-          <Plus className='mr-8 h-24 w-24' viewBox='0 1 24 24' />
-          에피그램 더보기
-        </Button>
-      </div>
+      {visibleCount < cards.length && (
+        <div className='pt:56 flex items-center justify-center pb-114 xl:pt-80'>
+          <Button variant='round' color='white' onClick={handleLoadMore}>
+            <Plus className='mr-8 h-24 w-24' viewBox='0 1 24 24' />
+            에피그램 더보기
+          </Button>
+        </div>
+      )}
       <div className='fixed bottom-104 right-24 md:bottom-92 md:right-72 xl:bottom-80 xl:right-120'>
         <div className='grid justify-items-end'>
           <Button
