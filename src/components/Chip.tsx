c53ec1d4e5e1650dec924292f5b3eb531d072a5b
KingNono1030 <hoejy0821@gmail.com>
2024-09-15 17:02:48 +0900

    remote/key props 제거

--- src/components/Chip.tsx
+++ src/components/Chip.tsx
@@ -2,20 +2,12 @@ import type { Key, LiHTMLAttributes } from 'react';
 import { twMerge } from 'tailwind-merge';
 
 interface ChipProps
-  extends Pick<LiHTMLAttributes<HTMLLIElement>, 'children' | 'className'> {
-  key?: Key;
-}
+  extends Pick<LiHTMLAttributes<HTMLLIElement>, 'children' | 'className'> {}
 
-export default function Chip({ children, className, key }: ChipProps) {
-  const chipClass = twMerge('', className);
-  return (
-    <li
-      key={key}
-      className={
-        'rounded-20 flex-center h-fit w-fit bg-background-100 px-12 py-8 text-xl leading-32 text-black-300 xl:px-14 xl:py-12 xl:text-2xl'
-      }
-    >
-      {children}
-    </li>
+export default function Chip({ children, className }: ChipProps) {
+  const chipClass = twMerge(
+    'rounded-20 flex-center h-fit w-fit bg-background-100 px-12 py-8 text-xl leading-32 text-black-300 xl:px-14 xl:py-12 xl:text-2xl',
+    className
   );
+  return <li className={chipClass}>{children}</li>;
 }
