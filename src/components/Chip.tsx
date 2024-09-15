3828c2eafd7fcacc1be0f6ff17541dc32a487afb
KingNono1030 <hoejy0821@gmail.com>
2024-09-15 14:29:29 +0900

    feat/chip 컴포넌트 스타일링 추가 + key props 수신

--- src/components/Chip.tsx
+++ src/components/Chip.tsx
@@ -1,18 +1,21 @@
-import { HTMLAttributes } from 'react';
+import type { Key, LiHTMLAttributes } from 'react';
 import { twMerge } from 'tailwind-merge';
 
 interface ChipProps
-  extends Pick<HTMLAttributes<HTMLSpanElement>, 'children' | 'className'> {}
+  extends Pick<LiHTMLAttributes<HTMLLIElement>, 'children' | 'className'> {
+  key?: Key;
+}
 
-export default function Chip({ children, className }: ChipProps) {
+export default function Chip({ children, className, key }: ChipProps) {
   const chipClass = twMerge('', className);
   return (
-    <span
+    <li
+      key={key}
       className={
-        'rounded-20 bg-background-100 px-12 py-8 text-xl leading-32 text-black-300 xl:px-14 xl:py-12 xl:text-2xl'
+        'rounded-20 flex-center h-fit w-fit bg-background-100 px-12 py-8 text-xl leading-32 text-black-300 xl:px-14 xl:py-12 xl:text-2xl'
       }
     >
       {children}
-    </span>
+    </li>
   );
 }
