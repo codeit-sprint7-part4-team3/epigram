a746fb61a26a0c888488c663f66372abf446a64e
KingNono1030 <hoejy0821@gmail.com>
2024-09-25 14:47:13 +0900

    feat/로그인 폼에 로그인 함수 연결

--- src/pages/signin/components/SignInForm.tsx
+++ src/pages/signin/components/SignInForm.tsx
@@ -1,11 +1,29 @@
+import { signinUser } from '@/api/auth/authApi';
 import Form from '@/components/Form';
 import { useForm } from 'react-hook-form';
+import { useMutation } from 'react-query';
 
-export default function LoginForm() {
+export default function SignInForm() {
   const methods = useForm();
+  const { setError } = methods;
+  const mutation = useMutation(signinUser, {
+    onSuccess: data => {
+      // 로그인 성공 시
+      // 1. 리다이렉트
+      // 2. 유저 정보 저장
+      console.log(data);
+    },
+    onError: (error: any) => {
+      const errorMessage = error.response.data.message;
+      const errorDetail = Object.keys(error.response.data.details)[0];
+      setError(errorDetail, { message: errorMessage });
+    },
+  });
+
   return (
     <Form
-      onSubmit={() => {
+      onSubmit={(data: SignInRequestBody) => {
+        mutation.mutate(data);
         console.log('로그인 폼 제출');
       }}
       methods={methods}
