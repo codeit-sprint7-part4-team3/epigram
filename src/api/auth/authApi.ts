065d34a2f2710bbc3064c06be471d40b389fff19
KingNono1030 <hoejy0821@gmail.com>
2024-09-25 14:44:35 +0900

    feat/로그인 api 함수

--- src/api/auth/authApi.ts
+++ src/api/auth/authApi.ts
@@ -1,7 +1,11 @@
 import instance from '@/api/comments/axios';
+import axios from 'axios';
 
 const signinUser = async (data: SignInRequestBody) => {
-  const response = await instance.post('auth/signIn', data);
+  const response = await axios.post('/api/auth/signIn', data, {
+    headers: { 'Content-Type': 'application/json' },
+    withCredentials: true,
+  });
   return response.data;
 };
 
