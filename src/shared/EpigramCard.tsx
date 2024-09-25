1cd5deca564151f644c3cedee481fe57725c6b22
soyoung <ssh0040233@gmail.com>
2024-09-25 10:29:33 +0900

    refactor/에피그램 카드 width variant 설정

--- src/shared/EpigramCard.tsx
+++ src/shared/EpigramCard.tsx
@@ -1,19 +1,34 @@
+import clsx from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
 interface CardProps {
   content: string;
   author: string;
   tags: string[];
+  variant?: EpigramVariant;
 }
 
-export default function EpigramCard({ content, author, tags }: CardProps) {
+type EpigramVariant = 'normal' | 'feed';
+
+export default function EpigramCard({
+  content,
+  author,
+  tags,
+  variant = 'normal',
+}: CardProps) {
+  const EpigramCardStyle = twMerge(
+    'stripe-pattern mb-8 flex flex-col justify-between rounded-2xl bg-blue-100 px-24 py-23',
+    styleByVariant[variant]
+  );
   return (
-    <div className='transition-animation font-secondary md:w-294 xl:w-585'>
-      <div className='stripe-pattern mb-8 flex min-h-295 flex-col justify-between rounded-2xl bg-blue-100 px-24 py-23'>
+    <div className='transition-animation font-secondary'>
+      <div className={EpigramCardStyle}>
         <div className='flex-1'>
           <div className='text-14 font-normal leading-24 md:text-16 md:leading-26 xl:text-24 xl:leading-40'>
             {content}
           </div>
         </div>
-        <div className='text-right text-14 font-normal leading-24 text-blue-400 md:text-16 md:leading-26 xl:text-24 xl:leading-40'>
+        <div className='mt-20 text-right text-14 font-normal leading-24 text-blue-400 md:text-16 md:leading-26 xl:text-24 xl:leading-40'>
           - {author} -
         </div>
       </div>
@@ -24,3 +39,8 @@ export default function EpigramCard({ content, author, tags }: CardProps) {
     </div>
   );
 }
+
+const styleByVariant: Record<EpigramVariant, string> = {
+  feed: 'md:w-294 xl:w-585 xl:min-h-295 md:min-h-180 min-h-140',
+  normal: 'xl:w-640 md:w-384 w-312',
+};
