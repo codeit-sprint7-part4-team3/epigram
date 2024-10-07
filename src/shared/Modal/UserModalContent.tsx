d5ac140dd6db4222413ec28b77176ef8e7bd6899
KingNono1030 <hoejy0821@gmail.com>
2024-10-08 00:06:30 +0900

    feat/유저 모달 추가

---
+++ src/shared/Modal/UserModalContent.tsx
@@ -0,0 +1,38 @@
+import Close from '@/assets/icons/ic-close.svg';
+import Button from '@/components/Button';
+import useModalStore from '@/lib/store/useModalStore';
+
+interface Props {
+  writer: Writer;
+}
+
+export default function UserModalContent({ writer }: Props) {
+  const { closeModal } = useModalStore();
+  const { image, nickname, id } = writer;
+
+  return (
+    <div
+      className={
+        'flex w-328 flex-col items-center rounded-24 bg-background-100 px-24 pb-24 pt-16 text-center xl:w-360 xl:px-40 xl:pb-32 xl:pt-24'
+      }
+    >
+      <button
+        onClick={closeModal}
+        className={'mb-8 self-end md:mb-24'}
+        type={'button'}
+      >
+        <Close className={'h-20 w-20 text-blue-500'} />
+      </button>
+      <div className={'mb-24'}></div>
+      <div>
+        <span
+          className={
+            'text-base font-semibold leading-26 text-black-400 xl:text-xl xl:leading-32'
+          }
+        >
+          {nickname}
+        </span>
+      </div>
+    </div>
+  );
+}
