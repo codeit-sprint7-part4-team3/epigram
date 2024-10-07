f05507ff98adb9cce961eac7c6066d69f9265d34
KingNono1030 <hoejy0821@gmail.com>
2024-10-07 12:01:05 +0900

    refactor/로그인 회원가입 핸들러 공통화

--- src/pages/api/auth/signIn.ts
+++ src/pages/api/auth/signIn.ts
@@ -1,40 +1,9 @@
-import instance from '@/api/comments/axios';
-import { serialize } from 'cookie';
+import { handleAuth } from '@/pages/api/auth/handleAuth';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-export default async function handler(
+export default async function signInHandler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method === 'POST') {
-    const data: SignInRequestBody = req.body;
-
-    try {
-      const response = await instance.post<SignInResponse>('auth/signIn', data);
-
-      const { user, accessToken, refreshToken } = response.data;
-      res.setHeader('Set-Cookie', [
-        serialize('accessToken', accessToken, {
-          httpOnly: true,
-          secure: process.env.NODE_ENV !== 'development',
-          maxAge: 60 * 30,
-          sameSite: 'strict',
-          path: '/',
-        }),
-        serialize('refreshToken', refreshToken, {
-          httpOnly: true,
-          secure: process.env.NODE_ENV !== 'development',
-          maxAge: 60 * 60 * 24 * 7,
-          sameSite: 'strict',
-          path: '/',
-        }),
-      ]);
-      res.status(200).json({ message: '로그인에 성공했습니다.', user });
-    } catch (error: any) {
-      const errorStatus = error.response?.status || 500;
-      res.status(errorStatus).json(error.response?.data);
-    }
-  } else {
-    res.status(405).json({ message: 'Method not allowed' });
-  }
+  return handleAuth(req, res, 'auth/signIn', '로그인에 성공했습니다.');
 }
