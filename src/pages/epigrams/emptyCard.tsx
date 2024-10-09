b12ae7229a9a3edee188ccf5bfa91f650340183a
soyoung <ssh0040233@gmail.com>
2024-10-10 02:23:20 +0900

    refactor/emptyCard text center정렬

--- src/pages/epigrams/emptyCard.tsx
+++ src/pages/epigrams/emptyCard.tsx
@@ -6,7 +6,7 @@ export default function EmptyCard() {
   return (
     <div className='flex-center mb-8 h-fit w-314 flex-col rounded-2xl bg-white bg-stripe-pattern bg-stripe-size p-30 md:w-384 xl:w-640'>
       <Empty className='h-110 w-110 xl:h-150 xl:w-150' />
-      <div className='mb-20 font-secondary text-14 text-zinc-700 xl:text-18'>
+      <div className='mb-20 text-center font-secondary text-14 text-zinc-700 xl:text-18'>
         오늘 등록된 에피그램이 없습니다
         <br /> 첫 번째 에피그램을 남겨주세요!
       </div>
