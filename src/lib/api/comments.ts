e30a8b69061381dda9ab5f62b07adfad91ce5bce
soyoung <ssh0040233@gmail.com>
2024-10-09 18:50:08 +0900

    fix/전체 댓글 가져오기 api 코드 추가

--- src/lib/api/comments.ts
+++ src/lib/api/comments.ts
@@ -9,4 +9,22 @@ const createComments = async (formData: CreateCommentBody) => {
   return response;
 };
 
-export { createComments };
+// 전체 댓글 가져오기
+const fetchEpigramComments = async ({
+  id: epigramId,
+  limit,
+  cursor = 0,
+}: BasicQuery) => {
+  try {
+    const response = await apiRequestWithAtuh({
+      endpoint: `/epigrams/${epigramId}/comments?limit=${limit}`,
+      method: 'GET',
+    });
+
+    console.log('응답 데이터:', response);
+  } catch (error) {
+    console.error('댓글 가져오기 실패:', error);
+  }
+};
+
+export { createComments, fetchEpigramComments };
